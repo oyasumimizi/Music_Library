7fcae460422c0f57a98ed49ad57fe7e2706b3981
unknown <daisyrakionvideos@gmail.com>
2020-11-17 20:25:28 -0600

    index title fixed, starteed working on the ajaxsetup, currently have a syntaxError but will fix later.

--- musiclibrary.js
+++ musiclibrary.js
@@ -1,1 +1,23 @@
-use strict;
+"use strict";
+
+$.ajaxSetup({
+    url: "http://www.devcodecampmusiclibrary.com/api/music",
+    dataType: "json",
+    type: "get",
+    success: function (data, textStatus, jQxhr) {
+    },
+    error: function (jqXhr, textStatus, errorThrown) {
+        console.log(errorThrown);
+    },
+})
+
+$.getJSON("Music Project.postman_collection.json", function (result) {
+    for (i = i, i < index.length, i++) {
+        $("div").append(result+ " ");
+
+    });
+    });
+});
+
+
+
